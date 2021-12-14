7f262d090c176cdf0f441b27a95d311a2bbe784d
Daniel Ale <danielale9291@gmail.com>
2021-12-14 11:22:57 +0100

    update

--- src/Application/DataTransferObjects/EmployeeDto.cs
+++ src/Application/DataTransferObjects/EmployeeDto.cs
@@ -1,3 +1,4 @@
 namespace Application.DataTransferObjects;
 
 public record EmployeeDto(Guid Id, string FirstName, string LastName, string Position);
+public record EmployeeForCreation(string FirstName, string LastName, int Age, string Position);
